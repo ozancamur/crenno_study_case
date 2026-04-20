f095522f22ada2bad712caffddb1b406b725f253
Ozan Camur <72649916+ozancamur@users.noreply.github.com>
2026-04-21 00:11:15 +0300

    config updated

--- lib/core/config/router.dart
+++ lib/core/config/router.dart
@@ -1,34 +1,30 @@
-import 'package:crenno_study_case/core/config/router_paths.dart';
-import 'package:crenno_study_case/core/di/app_dependencies.dart';
-import 'package:crenno_study_case/features/claim/presentation/view/claim_form_view.dart';
-import 'package:crenno_study_case/features/dashboard/domain/entities/policy.dart';
-import 'package:crenno_study_case/features/dashboard/presentation/view/dashboard_view.dart';
-import 'package:crenno_study_case/features/policy_detail/presentation/view/policy_detail_view.dart';
 import 'package:go_router/go_router.dart';
 
-GoRouter router(AppDependencies dependencies) {
+import '../../features/claim/presentation/view/claim_form_view.dart';
+import '../../features/dashboard/domain/entities/policy.dart';
+import '../../features/dashboard/presentation/view/dashboard_view.dart';
+import '../../features/policy_detail/presentation/view/policy_detail_view.dart';
+import 'router_paths.dart';
+
+GoRouter router() {
   return GoRouter(
     initialLocation: RouterPaths.DASHBOARD,
     routes: [
       GoRoute(
         path: RouterPaths.DASHBOARD,
-        builder: (context, state) =>
-            DashboardView(getPolicies: dependencies.getPolicies),
+        builder: (context, state) => DashboardView(),
       ),
       GoRoute(
         path: RouterPaths.POLICY,
         builder: (context, state) {
-          final policy = state.extra! as Policy;
+          final policy = state.extra as Policy;
           return PolicyDetailView(policy: policy);
         },
       ),
       GoRoute(
         path: RouterPaths.CLAIM,
         builder: (context, state) {
-          return ClaimFormView(
-            policy: state.extra! as Policy,
-            submitClaim: dependencies.submitClaim,
-          );
+          return ClaimFormView(policy: state.extra as Policy);
         },
       ),
     ],
--- lib/core/config/router_paths.dart
+++ lib/core/config/router_paths.dart
@@ -2,6 +2,10 @@ class RouterPaths {
   RouterPaths._();
 
   static const String DASHBOARD = "/";
+
   static const String POLICY = "/policy/:id";
+  static String TO_POLICY(String pId) => "/policy/$pId";
+
   static const String CLAIM = "/policy/:id/claim";
+  static String TO_CLAIM(String pId) => "/policy/$pId/claim";
 }
