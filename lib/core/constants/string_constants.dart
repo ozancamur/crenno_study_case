cbf0ffb10462b2eab7a1b88f1bc6e57ca3e001a3
Ozan Camur <72649916+ozancamur@users.noreply.github.com>
2026-04-20 19:57:39 +0300

    strings updated

---
+++ lib/core/constants/string_constants.dart
@@ -0,0 +1,29 @@
+final class StringConstants {
+  const StringConstants._();
+
+  static const appTitle = 'app_title';
+
+  static const dashboardTitle = 'dashboard_title';
+  static const dashboardEmptyTitle = 'dashboard_empty_title';
+  static const dashboardEmptyDescription = 'dashboard_empty_description';
+  static const dashboardErrorTitle = 'dashboard_error_title';
+  static const dashboardErrorDescription = 'dashboard_error_description';
+
+  static const commonRefresh = 'common_refresh';
+  static const commonTryAgain = 'common_try_again';
+
+  static const claimFormTitle = 'claim_form_title';
+  static const claimPolicyLabel = 'claim_policy_label';
+  static const claimSelectIncidentDate = 'claim_select_incident_date';
+  static const claimIncidentDateRequired = 'claim_incident_date_required';
+  static const claimIncidentDescriptionLabel =
+      'claim_incident_description_label';
+  static const claimIncidentDescriptionRequired =
+      'claim_incident_description_required';
+  static const claimSubmitButton = 'claim_submit_button';
+
+  static const policyTitle = 'policy_title';
+  static const policyStartDate = 'policy_start_date';
+  static const policyEndDate = 'policy_end_date';
+  static const policyCoverage = 'policy_coverage';
+}
