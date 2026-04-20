9b45588af2219d07544e1bed89885be1a4793382
Ozan Camur <72649916+ozancamur@users.noreply.github.com>
2026-04-21 00:11:06 +0300

    components updated

--- lib/app.dart
+++ lib/app.dart
@@ -1,29 +1,15 @@
 import 'package:crenno_study_case/core/config/router.dart';
 import 'package:crenno_study_case/core/constants/string_constants.dart';
-import 'package:crenno_study_case/core/di/app_dependencies.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-class InsuranceApp extends StatefulWidget {
+class InsuranceApp extends StatelessWidget {
   const InsuranceApp({super.key});
-
-  @override
-  State<InsuranceApp> createState() => _InsuranceAppState();
-}
-
-class _InsuranceAppState extends State<InsuranceApp> {
-  late final AppDependencies _dependencies;
-
-  @override
-  void initState() {
-    super.initState();
-    _dependencies = AppDependencies.create();
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      routerConfig: router(_dependencies),
+      routerConfig: router(),
       debugShowCheckedModeBanner: false,
       title: StringConstants.appTitle,
       localizationsDelegates: context.localizationDelegates,
---
+++ lib/core/components/custom_state_card.dart
@@ -0,0 +1,49 @@
+import 'package:crenno_study_case/core/components/custom_text.dart';
+import 'package:crenno_study_case/core/utils/context_extension.dart';
+import 'package:flutter/material.dart';
+
+class CustomStateCard extends StatelessWidget {
+  const CustomStateCard({
+    super.key,
+    required this.title,
+    required this.description,
+    this.actionLabel,
+    this.onAction,
+  });
+
+  final String title;
+  final String description;
+  final String? actionLabel;
+  final VoidCallback? onAction;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(context.width * 0.05),
+      child: Padding(
+        padding: EdgeInsets.all(context.width * 0.04),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          spacing: context.height * 0.01,
+          children: [
+            CustomText(
+              text: title,
+              weight: FontWeight.w600,
+              align: TextAlign.center,
+            ),
+            CustomText(text: description, align: TextAlign.center),
+            if (actionLabel != null && onAction != null) ...[
+              FilledButton(
+                onPressed: onAction,
+                child: CustomText(
+                  text: actionLabel!,
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+              ),
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+}
