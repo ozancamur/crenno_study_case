7b41cafda0fd57fa9053ae31c2de76dcfd74be8c
Ozan Camur <72649916+ozancamur@users.noreply.github.com>
2026-04-20 18:12:36 +0300

    constants

---
+++ lib/core/constants/font_size_enum.dart
@@ -0,0 +1,9 @@
+enum FontSizeEnum {
+  small([12, 11, 10]),
+  medium([15, 14, 13]),
+  large([18, 17, 16]),
+  extraLarge([21, 20, 19]);
+
+  final List<double> value;
+  const FontSizeEnum(this.value);
+}
